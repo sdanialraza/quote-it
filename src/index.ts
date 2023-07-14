27126c46a2744790d6390278464e0405ccffa8f8
Danial Raza <danialrazafb@gmail.com>
2023-07-14 05:10:04 +0200

    feat: introduce get quotes route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { config } from "dotenv"
 import cors from "cors"
 import express from "express"
 
+import router from "./routes"
+
 config()
 
 if (!process.env.PORT) process.exit(1)
@@ -12,5 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use(router)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express"
 
+import getQuotes from "./controllers/getQuotes"
+
 const router = Router()
+
+router.get("/quotes", getQuotes)
 
 export default router
