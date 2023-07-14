66e4de2f622475bb36b5c00a5fb8bd0cb3373f15
Danial Raza <danialrazafb@gmail.com>
2023-07-14 05:11:14 +0200

    feat: introduce get random quote route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 
 import getQuotes from "./controllers/getQuotes"
+import getRandomQuote from "./controllers/getRandomQuote"
 
 const router = Router()
 
 router.get("/quotes", getQuotes)
+router.get("/quotes/random", getRandomQuote)
 
 export default router
