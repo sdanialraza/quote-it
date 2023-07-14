416a97cf2b26c5463dcb7a09f774d79631f7018d
Danial Raza <danialrazafb@gmail.com>
2023-07-14 05:11:54 +0200

    feat: introduce get quote by id route

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express"
 
 import getQuotes from "./controllers/getQuotes"
 import getRandomQuote from "./controllers/getRandomQuote"
+import getQuoteById from "./controllers/getQuoteById"
 
 const router = Router()
 
 router.get("/quotes", getQuotes)
 router.get("/quotes/random", getRandomQuote)
+router.get("/quotes/:id", getQuoteById)
 
 export default router
