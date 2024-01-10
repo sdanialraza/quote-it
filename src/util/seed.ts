dd05fd2f396ac2e654f79a48a6cd8826703ff74a
Danial Raza <danialrazafb@gmail.com>
2024-01-10 16:35:20 +0100

    refactor: improve seeding logic

--- src/util/seed.ts
+++ src/util/seed.ts
@@ -1,17 +1,39 @@
+import process from "node:process"
+import { PrismaClient } from "@prisma/client"
 import { quotes } from "../data/quotes.js"
-import { database } from "../index.js"
 import { convertPropertiesForDatabase } from "./index.js"
 
-const existingQuotes = await database.quote.findMany()
+const database = new PrismaClient()
 
-const convertedQuotes = quotes.map(convertPropertiesForDatabase)
+async function seed() {
+  console.info("Started seeding the database with quotes...")
 
-for (const quote of convertedQuotes) {
-  if (existingQuotes.some(existingQuote => existingQuote.text === quote.text)) {
-    continue
+  let [added, skipped] = [0, 0]
+
+  const existingQuotes = await database.quote.findMany()
+
+  const convertedQuotes = quotes.map(convertPropertiesForDatabase)
+
+  for (const quote of convertedQuotes) {
+    if (existingQuotes.some(existingQuote => existingQuote.text === quote.text)) {
+      skipped++
+      continue
+    }
+
+    await database.quote.create({ data: quote })
+    added++
   }
 
-  await database.quote.create({ data: quote })
+  console.info("Finished seeding the database with quotes:")
+  console.info(`Added ${added} quotes and skipped ${skipped} quotes.`)
 }
 
-console.info("Seeded the database with quotes")
+await seed()
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await database.$disconnect()
+    process.exit(0)
+  })
