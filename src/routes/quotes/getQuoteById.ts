132a180be5debceb2bc005ea92343718280f0e55
Danial Raza <danialrazafb@gmail.com>
2024-01-11 23:23:29 +0100

    refactor: remove an unnecessary check
    This leads to an unnecessary query and may have some false positives.

--- src/routes/quotes/getQuoteById.ts
+++ src/routes/quotes/getQuoteById.ts
@@ -10,14 +10,8 @@ export default async function getQuoteById(request: Request, response: Response)
   try {
     const id = Number.parseInt(request.params.id, 10)
 
-    const totalQuotes = await database.quote.count({ where: { verified: true } })
-
     if (Number.isNaN(id) || id < 1) {
       return response.status(400).json({ message: "The provided id is invalid." })
-    }
-
-    if (id > totalQuotes) {
-      return response.status(404).json({ message: "The quote with the provided id does not exist." })
     }
 
     const quote: Quote | null = await database.quote.findUnique({ where: { id, verified: true } })
