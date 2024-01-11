83081c60b7dfbdc7ed7773d0c83a3707e43f3ad4
Danial Raza <danialrazafb@gmail.com>
2024-01-11 22:45:06 +0100

    fix: remove default rate limit in favor of post
    I am not sure how to implement rate limits on specific routes so for now there will only be a rate limit on post as that can be easily abused.

--- src/util/rateLimit.ts
+++ src/util/rateLimit.ts
@@ -1,10 +1,10 @@
 import rateLimit from "express-rate-limit"
 
-export const defaultRateLimit = rateLimit({
-  limit: 10,
+export const postRateLimit = rateLimit({
+  limit: 1,
   handler: (_request, response) => {
     response.status(429).json({
-      message: "Too many requests. The rate limit is 10 requests per minute on this route.",
+      message: "Too many requests. The rate limit is 1 requests per minute on this route.",
     })
   },
 })
