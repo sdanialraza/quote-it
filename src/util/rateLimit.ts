a09f075e33d4814c39b28517b452d3eb79903498
Danial Raza <danialrazafb@gmail.com>
2024-01-11 23:14:37 +0100

    chore: increase rate limit
    Having such a strict rate limit can hinder legitimate usage.

--- src/util/rateLimit.ts
+++ src/util/rateLimit.ts
@@ -1,10 +1,10 @@
 import rateLimit from "express-rate-limit"
 
 export const postRateLimit = rateLimit({
-  limit: 1,
+  limit: 5,
   handler: (_request, response) => {
     response.status(429).json({
-      message: "Too many requests. The rate limit is 1 requests per minute on this route.",
+      message: "Too many requests. The rate limit is 5 requests per minute on this route.",
     })
   },
 })
